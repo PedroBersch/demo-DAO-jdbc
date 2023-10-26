29f39a26750dd2c93a20c1eb1af1fc67c210fcb8
Pedro Bersch <pedroleottebersch@gmail.com>
2023-10-26 15:41:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -10,6 +13,8 @@ public class Program {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Department dp = new Department(1,"Books");
         Seller seller = new Seller(1,"Pedro", "pedro@gmail.com",sdf.parse("16/04/2003"), 2000.0,dp);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(dp);
         System.out.println(seller);
     }
