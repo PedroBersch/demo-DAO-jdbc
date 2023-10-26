7eeed5e835f78ab8f7fa852c842ef5d3cb5f94ac
Pedro Bersch <pedroleottebersch@gmail.com>
2023-10-26 17:05:42 -0300

    Creating findById method

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,13 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 public class Program {
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Department dp = new Department(1,"Books");
-        Seller seller = new Seller(1,"Pedro", "pedro@gmail.com",sdf.parse("16/04/2003"), 2000.0,dp);
+    public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(dp);
         System.out.println(seller);
     }
 }
